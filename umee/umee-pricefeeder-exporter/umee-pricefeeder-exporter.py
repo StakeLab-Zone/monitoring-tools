73d9e8650477862c5522a391fe460371cda0fa0b
Charles JUDITH <contact@charlesjudith.com>
2023-12-22 22:43:04 +0100

    Update umee-pricefeeder-exporter.py

--- umee/umee-pricefeeder-exporter/umee-pricefeeder-exporter.py
+++ umee/umee-pricefeeder-exporter/umee-pricefeeder-exporter.py
@@ -10,7 +10,7 @@ from prometheus_client import Counter, start_http_server, Gauge
 REQUESTS_TOTAL = Counter('requests_total', 'Total number of requests made')
 ERRORS_TOTAL = Counter('errors_total', 'Total number of errors encountered')
 UMEE_VOTES_GAUGE = Gauge('umee_missing_votes', 'Umee missing votes')
-CHECK_INTERVAL = os.getenv('CHECK_INTERVAL', 300)
+CHECK_INTERVAL = int(os.getenv('CHECK_INTERVAL', 300))
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
